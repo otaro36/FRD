3059fb4bd15be57bdb47aeae6bd31b7881f50559
otaro36 <68829944+otaro36@users.noreply.github.com>
2021-10-01 23:48:31 -0500

    combinaciones

--- Habita4.0/Assets/Script/Impresora3d.cs
+++ Habita4.0/Assets/Script/Impresora3d.cs
@@ -1,36 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Impresora3d : MonoBehaviour
-{
-    public GameObject carcasa;
+{   //Instancia elementos de la mesa
     public GameObject aglinato;
-    public GameObject fabrica;
-    public GameObject destruido;
-    public Transform instanciaCarcasa;
-    public Transform instanciaAglinato;
+    public GameObject grafeno;
+    public GameObject antena5G;
     public Transform instanciaGrafeno;
+    public Transform instanciaAglinato;
     public Transform instanciaAntena;
+    //Instancia elemento en la impresora
+    public GameObject carcasa;
+    public GameObject olivo;
+    public Transform instanciaImpresora3D;
+    //Asignacion a elementos vacios en colision
+    public GameObject colliAlga;
+    public GameObject colliGrafeno;
+    public GameObject colliDestruido;
+
+    public GameObject destruido;
+    public Slider tiempoCreacion;
     public int objetoCreado;
+    public int objeto1;
+    public int objeto2;
+    public int objeto3;
+    public int objeto4;
+    public int impresiones;
+    public float timePreparacion;
     public float time;
     private void Update()
     {
-        switch (objetoCreado)
+        objetoCreado = objeto1 + objeto2;
+        if (objetoCreado!=0)
         {
-            case 1:            
-                StartCoroutine("Impresion");
-                Instantiate(aglinato, instanciaAglinato.position, Quaternion.identity);
-                objetoCreado = 0;
-                break;
+            StartCoroutine("Convinaciones");
+            switch (impresiones)
+            {
+                case 1:
+                    StartCoroutine("Impresion");
+                    objeto1 = 0;
+                    objeto2 = 0;
+                    objetoCreado = 0;
+                    break;
+                case 3:
+                    StartCoroutine("Olivo");
+                    objeto1 = 0;
+                    objeto2 = 0;
+                    objetoCreado = 0;
+                    break;
+            }
+            
+
         }
+        print(0);
+        
+
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag== "Alga")
         {
-            fabrica = other.gameObject;
-            objetoCreado = 1;
+            colliAlga = other.gameObject;
+            objeto1 = 1;
+        }
+        else if (other.tag== "Grafeno")
+        {
+            colliGrafeno = other.gameObject;
+            objeto2 = 2;
         }
         else if (other.tag!="Player"&&other.tag!="Impresora3d")
         {
@@ -38,13 +76,28 @@ public class Impresora3d : MonoBehaviour
             Destroy(other.gameObject);
         }
     }
+    IEnumerator Convinaciones()
+    {
+        yield return new WaitForSecondsRealtime(10f);
+        impresiones = objetoCreado;
+        print(1);
+    }
     IEnumerator Impresion()
     {
-
-        yield return new WaitForSecondsRealtime(time);
-        Destroy(fabrica.gameObject);
+        Destroy(colliAlga.gameObject);
         yield return new WaitForSecondsRealtime(0.2f);
-        Instantiate(carcasa, instanciaCarcasa.position, Quaternion.identity);
+        Instantiate(carcasa, instanciaImpresora3D.position, Quaternion.identity);
         yield return new WaitForSecondsRealtime(time);
+        Instantiate(aglinato, instanciaAglinato.position, Quaternion.identity);
+    }
+    IEnumerator Olivo()
+    {
+        Destroy(colliGrafeno.gameObject);
+        Destroy(colliAlga.gameObject);
+        yield return new WaitForSecondsRealtime(0.2f);
+        Instantiate(olivo, instanciaImpresora3D.position, Quaternion.identity);
+        yield return new WaitForSecondsRealtime(time);
+        Instantiate(aglinato, instanciaAglinato.position, Quaternion.identity);
+        Instantiate(aglinato, instanciaAglinato.position, Quaternion.identity);
     }
 }
---
+++ Habita4.0/Assets/Script/RelojGeneral.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RelojGeneral : MonoBehaviour
+{
+    public float tiempo;
+    public Text tiempotxt;
+    public GameObject player;
+    public GameObject canvas;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tiempo = 2000;
+        tiempotxt.text = "2:00";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalcularTiempo();
+        if (tiempo<=0)
+        {
+            tiempotxt.text = 0 + ":" + 0;
+            player.GetComponent<CharacterController>().enabled = false;
+            canvas.SetActive(true);
+        }
+    }
+    void CalcularTiempo()
+    {
+        tiempo -= Time.deltaTime;
+        int minutos = (int)tiempo / 60;
+        int segundos = (int)tiempo % 60;
+        tiempotxt.text = minutos.ToString() + ":" + segundos.ToString().PadLeft(2, '0');
+    }
+}
